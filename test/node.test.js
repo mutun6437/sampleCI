5b60af07aa4947c8e1844a52d75727455a394757
mutun6437 <higenjitukyoukai@gmail.com>
2015-06-27 16:55:05 +0900

    mtn_commit_0627.1655

---
+++ test/node.test.js
@@ -0,0 +1,13 @@
+var assert = require('assert');
+//var myModule = require('./myModule');
+
+console.log("テストします");
+/*
+describe('myModule', function () {
+    describe('greet', function () {
+        it('引数に応じて決まった文字列を返すこと', function () {
+            assert.equal(myModule.greet('taro'), 'Hello,taro');
+        });
+    });
+});
+*/
